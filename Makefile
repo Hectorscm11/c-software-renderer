f93451c88ddedcb1c04a02a8f7c51adb9ac28946
hector <hector@hector.home>
2026-04-26 13:21:52 +0200

    Drawing cube on the screen

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
-# Variables
 CC = gcc
 CFLAGS = -Wall -Wextra -std=c99 $(shell pkg-config --cflags sdl2)
 LIBS = $(shell pkg-config --libs sdl2)
 TARGET = renderer
 SRC = c-software-renderer.c
 
-# Regla principal
+
 all: $(TARGET)
 
 $(TARGET): $(SRC)
--- c-software-renderer.c
+++ c-software-renderer.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <SDL2/SDL.h>
+#include <math.h>
 
 #define WIDTH 1400
 #define HEIGHT 900
 
+#define PIXELS_PER_POINT 250
+
 typedef struct point{
-    int x;
-    int y;
+    float x;
+    float y;
+    float z;
 }point;
+
+
+point cube[8] = {
+    {-1, -1, -1}, {1, -1, -1}, {1, 1, -1}, {-1, 1, -1},
+    {-1, -1,  1}, {1, -1,  1}, {1, 1,  1}, {-1, 1,  1}  
+};
 
 
 static inline void draw_pixel(uint32_t* pixels, int x, int y, uint32_t color);
 void draw_line(uint32_t* pixels, point *a, point *b, uint32_t color);
+int project(point *v, point* p_v);
+void draw_cube(uint32_t* pixels, point* cube, uint32_t color);
+void traslate_cube(point* cube, point* cube_traslated);
+void rotate_cube(point* cube);
 
 
 
 int main(void){
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    //clock_t t_init, t_final;
+    //double sec;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("Error al inicializar SDL: %s\n", SDL_GetError());
         return 1;
     }
@@ -43,17 +61,17 @@ int main(void){
     );
 
 
-    point a;
-    a.x =300;
-    a.y=300;
 
-    point b;
-    b.x =50;
-    b.y=250;
+    //long long frames = 0;
 
+    uint32_t last_time = SDL_GetTicks(); 
+    uint32_t current_time;
+    uint32_t frame_count = 0;
 
     int running = 1;
     SDL_Event event;
+
+    //t_init = clock();
 
     while (running) {
         //events (close window)
@@ -61,18 +79,39 @@ int main(void){
             if (event.type == SDL_QUIT) running = 0;
         }
 
+
         for (int i = 0; i < WIDTH * HEIGHT; i++) pixels[i] = 0xFF000000;
-
-
       
 
-        draw_line(pixels, &a, &b, 0xFFFF0000);
+
+        rotate_cube(cube);
+        point cube_traslated[8];
+        traslate_cube(cube, cube_traslated);
+        draw_cube(pixels, cube_traslated, 0xFFFF0000);
 
         //update window
         SDL_UpdateTexture(color_buffer_texture, NULL, pixels, WIDTH * sizeof(uint32_t));
         SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
         SDL_RenderPresent(renderer);
+
+        frame_count++;
+
+        current_time = SDL_GetTicks();
+
+        if(current_time - last_time >= 1000){
+            printf("FPS:%d\n",frame_count);
+            frame_count = 0;
+            last_time = current_time;
+        }
+
+        //frames++;
     }
+
+    //t_final = clock();
+
+    //sec = (double)(t_final - t_init) / CLOCKS_PER_SEC;
+
+    //long long frames_per_sec = frames / (long long)sec;
 
     SDL_DestroyTexture(color_buffer_texture);
     SDL_DestroyRenderer(renderer);
@@ -80,6 +119,9 @@ int main(void){
 
     SDL_DestroyWindow(window);
     SDL_Quit();
+
+    //printf("Frames/sec = %lld\n", frames_per_sec);
+
     return 0;
 }
 
@@ -92,7 +134,7 @@ static inline void draw_pixel(uint32_t* pixels, int x, int y, uint32_t color) {
 
 
 void draw_line(uint32_t* pixels, point *a, point *b, uint32_t color){
-   int x0 = a->x;
+    int x0 = a->x;
     int y0 = a->y;
     int x1 = b->x;
     int y1 = b->y;
@@ -121,9 +163,52 @@ void draw_line(uint32_t* pixels, point *a, point *b, uint32_t color){
         if(e2 < dx){
             err += dx;
             y0 +=sy;
-        }
-      
+        } 
+    }
+}
+
+
+int project(point* v, point* p_v) {
+    if(v->z == 0){
+        return 1;
+    }
+    
+    p_v->x = (v->x / v->z) * PIXELS_PER_POINT + (WIDTH / 2);
+    p_v->y = (v->y / v->z) * PIXELS_PER_POINT + (HEIGHT / 2);
+
+    return 0;
+}
+
+void draw_cube(uint32_t* pixels, point* cube, uint32_t color){
+    point cube_projected[8];
+    for(int i = 0; i < 8; i++){
+        project(&cube[i], &cube_projected[i]);
     }
 
+    //front face
+    draw_line(pixels, &cube_projected[0], &cube_projected[1], color);
+    draw_line(pixels, &cube_projected[1], &cube_projected[2], color);
+    draw_line(pixels, &cube_projected[2], &cube_projected[3], color);
+    draw_line(pixels, &cube_projected[3], &cube_projected[0], color);
 
+    //back face
+    draw_line(pixels, &cube_projected[4], &cube_projected[5], 0xFF0000FF);
+    draw_line(pixels, &cube_projected[5], &cube_projected[6], 0xFF0000FF);
+    draw_line(pixels, &cube_projected[6], &cube_projected[7], 0xFF0000FF);
+    draw_line(pixels, &cube_projected[7], &cube_projected[4], 0xFF0000FF);
+
+    //faces conexions
+    draw_line(pixels, &cube_projected[0], &cube_projected[4], 0xFF00FF00);
+    draw_line(pixels, &cube_projected[1], &cube_projected[5], 0xFF00FF00);
+    draw_line(pixels, &cube_projected[2], &cube_projected[6], 0xFF00FF00);
+    draw_line(pixels, &cube_projected[3], &cube_projected[7], 0xFF00FF00);
 }
+
+void traslate_cube(point* cube, point* cube_traslated){
+    for(int i = 0; i < 8; i++){
+        cube_traslated[i] = cube[i];
+        cube_traslated[i].z += 3;
+    }
+}
+
+void rotate_cube(point* cube){}
